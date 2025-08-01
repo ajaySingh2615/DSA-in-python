04b6906dc4648b9e21dbad3d6363284c1d218eb7
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-08-01 11:19:50 +0530

    run all the trest cases for this code

--- 02_functions/03_prime.py
+++ 02_functions/03_prime.py
@@ -12,9 +12,16 @@ def isPrime(num):
     return True
 
 
-num = int(input("Enter a num to check prime or not: "))
+test_cases = [-10, 0, 1, 2, 3, 4, 5, 9, 11, 15, 17, 25, 29, 100, 101, 9973, 10000]
 
-if isPrime(num) == True:
-    print(f"{num} is a prime number")
-else:
-    print(f"{num} is not a prime number")
+for num in test_cases:
+    result = isPrime(num)
+    print(f"{num} : {'prime' if result else 'not prime'}")
+
+
+# num = int(input("Enter a num to check prime or not: "))
+
+# if isPrime(num) == True:
+#     print(f"{num} is a prime number")
+# else:
+#     print(f"{num} is not a prime number")
